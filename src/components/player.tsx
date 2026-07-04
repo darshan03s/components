6f9e0ccc29affa94d08a4c717307cd22e3a2672c
Darshan S <darshans2727@gmail.com>
2026-07-05 00:47:56 +0530

    add mediabunny

--- src/components/player.tsx
+++ src/components/player.tsx
@@ -3,6 +3,8 @@
 import { Info } from 'lucide-react'
 import { Card, CardContent, CardHeader, CardTitle } from './ui/card'
 import { HoverCard, HoverCardContent, HoverCardTrigger } from './ui/hover-card'
+import { getFileData, InputFileData } from '@/lib/mediabunny'
+import { useEffect, useState } from 'react'
 
 function formatBytes(bytes: number) {
   const sizes = ['B', 'KB', 'MB', 'GB']
@@ -10,14 +12,57 @@ function formatBytes(bytes: number) {
   return (bytes / Math.pow(1024, i)).toFixed(2) + ' ' + sizes[i]
 }
 
+function formatDuration(duration: number): string {
+  const totalSeconds = Math.floor(duration)
+
+  const hours = Math.floor(totalSeconds / 3600)
+  const minutes = Math.floor((totalSeconds % 3600) / 60)
+  const seconds = totalSeconds % 60
+
+  if (hours > 0) {
+    return `${hours.toString().padStart(2, '0')}:${minutes
+      .toString()
+      .padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`
+  }
+
+  if (minutes > 0) {
+    return `${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`
+  }
+
+  return seconds.toString()
+}
+
 const Player = ({ file }: { file: File }) => {
   const videoUrl = URL.createObjectURL(file)
+  const [fileData, setFileData] = useState<InputFileData | null>(null)
+
+  useEffect(() => {
+    getFileData(file).then(setFileData)
+  }, [file])
+
+  if (!fileData) return null
 
   const hoverCardContentMap = {
     'Last modified': new Date(file.lastModified).toLocaleDateString(),
     Size: formatBytes(file.size),
-    Type: file.type
+    Format: fileData.format.name,
+    'MIME Type': fileData.format.mimeType,
+    Duration: formatDuration(fileData.duration),
+    Artist: fileData.metadataTags.artist,
+    Album: fileData.metadataTags.album,
+    'Total tracks': fileData.metadataTags.tracksTotal,
+    Genre: fileData.metadataTags.genre
   }
+
+  const image = fileData.metadataTags.images?.[0]
+
+  const posterUrl = image
+    ? URL.createObjectURL(
+        new Blob([new Uint8Array(image.data)], {
+          type: image.mimeType
+        })
+      )
+    : undefined
 
   return (
     <Card>
@@ -30,18 +75,24 @@ const Player = ({ file }: { file: File }) => {
           </HoverCardTrigger>
           <HoverCardContent side="bottom" className="w-48">
             <div className="flex flex-col gap-2 text-xs">
-              {Object.entries(hoverCardContentMap).map(([key, value]) => (
-                <div key={key}>
-                  <span className="font-semibold">{key} : </span>
-                  <span>{value}</span>
-                </div>
-              ))}
+              {Object.entries(hoverCardContentMap).map(([key, value]) =>
+                value ? (
+                  <div key={key}>
+                    <span className="font-semibold">{key} : </span>
+                    <span>{value}</span>
+                  </div>
+                ) : null
+              )}
             </div>
           </HoverCardContent>
         </HoverCard>
       </CardHeader>
       <CardContent>
-        <video src={videoUrl} className="aspect-video min-w-100 w-100 rounded-md bg-transparent" />
+        <video
+          poster={posterUrl}
+          src={videoUrl}
+          className="aspect-video min-w-100 w-100 rounded-md bg-transparent outline"
+        />
       </CardContent>
     </Card>
   )
---
+++ src/lib/mediabunny.ts
@@ -0,0 +1,142 @@
+'use client'
+
+import {
+  ALL_FORMATS,
+  BlobSource,
+  Input,
+  InputAudioTrack,
+  InputTrack,
+  MediaCodec,
+  MetadataTags,
+  TrackDisposition
+} from 'mediabunny'
+
+export type InputFileData = {
+  audioTracks: InputAudioTrack[]
+  duration: number
+  computedDuration: number
+  format: {
+    name: string
+    mimeType: string
+  }
+  metadataTags: MetadataTags
+  mimeType: string
+  size: number | null
+  tracksData: TrackData[]
+}
+
+type TrackData = {
+  id: number
+  type: 'video' | 'audio' | 'subtitle'
+  averageBitrate: number
+  codec: MediaCodec | null
+  codecParamString: string | null
+  disposition: TrackDisposition
+  duration: number | null
+  lang: string
+  isAudio: boolean
+  isVideo: boolean
+}
+
+export async function getFileData(file: File): Promise<InputFileData> {
+  const input = new Input({
+    formats: ALL_FORMATS,
+    source: new BlobSource(file)
+  })
+
+  let data = {}
+
+  const [audioTracks, duration, format, metadataTags, mimeType, size] = await Promise.all([
+    input.getAudioTracks(),
+    input.getDurationFromMetadata(),
+    input.getFormat(),
+    input.getMetadataTags(),
+    input.getMimeType(),
+    input.source.getSizeOrNull()
+  ])
+
+  const computedDuration = await input.computeDuration()
+
+  data = {
+    audioTracks,
+    duration,
+    computedDuration,
+    format: { name: format.name, mimeType: format.mimeType },
+    metadataTags,
+    mimeType,
+    size
+  }
+
+  const tracksData = await getTracksData(input)
+
+  data = { ...data, tracksData }
+
+  return data as InputFileData
+}
+
+export async function getTrackData(track: InputTrack) {
+  const codec = await track.getCodec()
+  const codecParamString = await track.getCodecParameterString()
+  const disposition = await track.getDisposition()
+  const duration = await track.getDurationFromMetadata()
+  const lang = await track.getLanguageCode()
+  const isAudio = track.isAudioTrack()
+  const isVideo = track.isVideoTrack()
+  const stats = await track.computePacketStats(100)
+  const averageBitrate = stats.averageBitrate
+  let trackData = {
+    id: track.id,
+    type: track.type,
+    averageBitrate,
+    codec,
+    codecParamString,
+    disposition,
+    duration,
+    lang,
+    isAudio,
+    isVideo
+  }
+
+  if (track.isVideoTrack()) {
+    const codedHeight = await track.getCodedHeight()
+    const codedWidth = await track.getCodedWidth()
+    const colorSpace = await track.getColorSpace()
+    const displayHeight = await track.getDisplayHeight()
+    const displayWidth = await track.getDisplayWidth()
+    const rotation = await track.getRotation()
+    const frameRate = stats.averagePacketRate
+    const videoData = {
+      codedHeight,
+      codedWidth,
+      colorSpace,
+      displayHeight,
+      displayWidth,
+      rotation,
+      frameRate,
+      averageBitrate
+    }
+
+    trackData = { ...trackData, ...videoData }
+  } else if (track.isAudioTrack()) {
+    const sampleRate = await track.getSampleRate()
+    const channels = await track.getNumberOfChannels()
+    const audioData = { sampleRate, channels }
+
+    trackData = { ...trackData, ...audioData }
+  }
+
+  return trackData
+}
+
+export async function getTracksData(input: Input) {
+  const tracks = await input.getTracks()
+
+  const tracksData = []
+
+  for (const track of tracks) {
+    const trackData = await getTrackData(track)
+    tracksData.push(trackData)
+  }
+
+  return tracksData
+}
