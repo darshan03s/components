2e73165fbe0141b5f38ea57ac111c572ac1dcba8
Darshan S <darshans2727@gmail.com>
2026-07-04 23:15:01 +0530

    add hover card

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,6 +8,8 @@ import { useState } from 'react'
 const Page = () => {
   const [file, setFile] = useState<File | null>(null)
 
+  console.log(file)
+
   return (
     <Main className="flex items-center justify-center ">
       <div>{!file ? <FileImport setFile={setFile} /> : <Player file={file} />}</div>
--- src/components/player.tsx
+++ src/components/player.tsx
@@ -1,13 +1,44 @@
 'use client'
 
+import { Info } from 'lucide-react'
 import { Card, CardContent, CardHeader, CardTitle } from './ui/card'
+import { HoverCard, HoverCardContent, HoverCardTrigger } from './ui/hover-card'
+
+function formatBytes(bytes: number) {
+  const sizes = ['B', 'KB', 'MB', 'GB']
+  const i = Math.floor(Math.log(bytes) / Math.log(1024))
+  return (bytes / Math.pow(1024, i)).toFixed(2) + ' ' + sizes[i]
+}
 
 const Player = ({ file }: { file: File }) => {
   const videoUrl = URL.createObjectURL(file)
+
+  const hoverCardContentMap = {
+    'Last modified': new Date(file.lastModified).toLocaleDateString(),
+    Size: formatBytes(file.size),
+    Type: file.type
+  }
+
   return (
     <Card>
-      <CardHeader>
-        <CardTitle className="text-center">{file.name}</CardTitle>
+      <CardHeader className="flex flex-row items-center justify-between">
+        <div></div>
+        <CardTitle className="text-center line-clamp-2">{file.name}</CardTitle>
+        <HoverCard>
+          <HoverCardTrigger className="cursor-pointer hover:opacity-80 transition-opacity duration-200">
+            <Info className="size-4" />
+          </HoverCardTrigger>
+          <HoverCardContent side="bottom" className="w-48">
+            <div className="flex flex-col gap-2 text-xs">
+              {Object.entries(hoverCardContentMap).map(([key, value]) => (
+                <div key={key}>
+                  <span className="font-semibold">{key} : </span>
+                  <span>{value}</span>
+                </div>
+              ))}
+            </div>
+          </HoverCardContent>
+        </HoverCard>
       </CardHeader>
       <CardContent>
         <video src={videoUrl} className="aspect-video min-w-100 w-100 rounded-md bg-transparent" />
