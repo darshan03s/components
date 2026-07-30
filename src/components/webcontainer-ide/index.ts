9af585c6bb87fd6b13c9261dde3ed91198e185b3
Darshan S <darshans2727@gmail.com>
2026-07-30 12:14:05 +0530

    refactor(webcontainer-ide): export hooks from index file

--- src/components/webcontainer-ide/index.ts
+++ src/components/webcontainer-ide/index.ts
@@ -1,2 +1,3 @@
 export { WebContainerIDE } from './webcontainer-ide'
 export { WebContainerIDEProvider } from './providers/webcontainer-ide-provider'
+export { useFileSystem, useIde, useWebContainer } from './hooks'
