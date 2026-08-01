9e0f9cb405f9570fb91e6a5eb6556cf16431b34a
Darshan S <darshans2727@gmail.com>
2026-08-01 23:09:51 +0530

    remove heading

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,6 @@ const Page = () => {
   return (
     <Main className="flex items-center justify-center">
       <div className="flex flex-col gap-4">
-        <h1 className="text-2xl font-bold">Components</h1>
         <div className="flex flex-col items-center gap-2">
           {process.env.NODE_ENV === 'development' && (
             <Link href="/dev/webcontainer-ide/all" className="underline">
