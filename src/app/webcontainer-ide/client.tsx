c074e4709735aedde40981d246481c55fb007819
Darshan S <darshans2727@gmail.com>
2026-08-02 20:23:04 +0530

    fix(webcontainer-ide): correct prop name

--- src/app/webcontainer-ide/client.tsx
+++ src/app/webcontainer-ide/client.tsx
@@ -11,7 +11,7 @@ export const Client = () => {
     <Main className="flex items-center justify-center">
       <WebContainerIDE
         loadFromSnapshot="/api/snapshot?template=vite-tsx"
-        theme={resolvedTheme as 'light' | 'dark'}
+        editorTheme={resolvedTheme as 'light' | 'dark'}
       />
     </Main>
   )
