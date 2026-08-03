1c119ed4a542fa2e494a7e186e158e932493fb58
Darshan S <darshans2727@gmail.com>
2026-08-03 13:50:18 +0530

    update template

--- templates/vite-tsx/src/App.tsx
+++ templates/vite-tsx/src/App.tsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
 import './App.css'
-import heroImg from './assets/hero.png'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 
@@ -11,7 +10,6 @@ function App() {
     <>
       <section id="center">
         <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
           <img src={reactLogo} className="framework" alt="React logo" />
           <img src={viteLogo} className="vite" alt="Vite logo" />
         </div>
